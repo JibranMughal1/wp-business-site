25c58d159a5d370f670e220ef47df2d00347955e
zafarsanieuroshub <zafar.euroshub@gmail.com>
2025-12-30 02:02:17 +0500

    Design premium navigation bar with glassmorphism and sticky behavior

--- wp-content/themes/twentytwentyfive/patterns/header.php
+++ wp-content/themes/twentytwentyfive/patterns/header.php
@@ -12,8 +12,8 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull">
+<!-- wp:group {"align":"full","className":"premium-header","layout":{"type":"default"}} -->
+<div class="wp-block-group alignfull premium-header">
 	<!-- wp:group {"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group">
 		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
